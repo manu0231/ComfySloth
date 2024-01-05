17e8dca96777cd4e141b5f39116f79289dcaa1ca
= <mauryasourav998@gmail.com>
2024-01-05 22:42:28 +0530

    configSetting

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      // '/api': 'http://localhost:3000',
+      '/api': 'https://storeserver-production-bc7e.up.railway.app',
     },
   },
   plugins: [react()],
